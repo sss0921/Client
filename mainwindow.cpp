9dc9c8168521fc93e83041862ddd2302efd4326c
Sun.Songsong <sss09217313@163.com>
2019-05-07 23:40:17 +0800

    Add MainWindow class log
    Change-Id: Ie26ef7ab561838d7075525ff05521ee60bbe2a23

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QLoggingCategory>
+
+Q_LOGGING_CATEGORY(mainWindow, "Client.MainWindow")
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
